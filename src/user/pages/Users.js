6f806227cb250238773dc5d6b22d6b64f3fce26f
Anand-Theertha <anandtheerthav3@gmail.com>
2025-01-07 12:04:26 +0000

    UserList, UserItem and Card component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,22 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users</h2>;
+  const USERS = [
+    {
+      id: 1,
+      name: "Max",
+      image: "https://images.app.goo.gl/CZ2wXbcSTEY4C1vU6",
+      places: 4,
+    },
+    {
+      id: 1,
+      name: "Chris",
+      image: "https://images.app.goo.gl/q8DsnhAvspXeWxty6",
+      places: 4,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
