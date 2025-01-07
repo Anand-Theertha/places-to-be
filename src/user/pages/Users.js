6fb841302f2161958d08665ea09f9b4ef2aeaf40
Anand-Theertha <anandtheerthav3@gmail.com>
2025-01-07 13:17:39 +0000

    Navigation Links and Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import UsersList from "../components/UsersList";
 
 const Users = () => {
@@ -6,14 +7,14 @@ const Users = () => {
     {
       id: 1,
       name: "Max",
-      image: "https://images.app.goo.gl/CZ2wXbcSTEY4C1vU6",
+      image: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg",
       places: 4,
     },
     {
-      id: 1,
+      id: 2,
       name: "Chris",
-      image: "https://images.app.goo.gl/q8DsnhAvspXeWxty6",
-      places: 4,
+      image: "https://images.pexels.com/photos/91228/pexels-photo-91228.jpeg",
+      places: 6,
     },
   ];
   return <UsersList items={USERS} />;
